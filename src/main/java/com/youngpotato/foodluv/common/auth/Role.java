e40aa49450a66a2faa69926389ed9271a65f79c8
wujin <woojin97318@gmail.com>
2024-07-14 23:02:39 +0900

    refactor : common response 처리

--- src/main/java/com/youngpotato/foodluv/common/auth/Role.java
+++ src/main/java/com/youngpotato/foodluv/common/auth/Role.java
@@ -6,10 +6,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Role {
-    ROLE_ANONYMOUS("ROLE_ANONYMOUS"), // 미인증 사용자
-    ROLE_USER("ROLE_USER"), // 일반 사용자
-    ROLE_MANAGER("ROLE_MANAGER"), // 매니저
-    ROLE_ADMIN("ROLE_ADMIN"); // 관리자
+    ROLE_ANONYMOUS("ROLE_ANONYMOUS"),   // 미인증 사용자
+    ROLE_USER("ROLE_USER"),             // 일반 사용자
+    ROLE_MANAGER("ROLE_MANAGER"),       // 매니저
+    ROLE_ADMIN("ROLE_ADMIN");           // 관리자
 
     private final String role;
 }
--- src/main/java/com/youngpotato/foodluv/web/controller/MemberController.java
+++ src/main/java/com/youngpotato/foodluv/web/controller/MemberController.java
@@ -1,5 +1,7 @@
 package com.youngpotato.foodluv.web.controller;
 
+import com.youngpotato.foodluv.common.response.ResponseService;
+import com.youngpotato.foodluv.common.response.SingleResponse;
 import com.youngpotato.foodluv.service.MemberService;
 import com.youngpotato.foodluv.web.dto.*;
 import jakarta.validation.Valid;
@@ -12,30 +14,34 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1")
 public class MemberController {
 
+    private final ResponseService responseService;
     private final MemberService memberService;
 
     /**
      * 일반 회원가입
      */
     @PostMapping("/sign-up")
-    public ResponseEntity<MemberDTO> signup(@Valid @RequestBody SignUpDTO signUpDTO) {
-        return ResponseEntity.ok(memberService.signup(signUpDTO));
+    public SingleResponse<MemberDTO> signup(@Valid @RequestBody SignUpDTO signUpDTO) {
+        MemberDTO memberDTO = memberService.signup(signUpDTO);
+        return responseService.getSingleResponse(memberDTO);
     }
 
     /**
      * 로그인
      */
     @PostMapping("/sign-in")
-    public ResponseEntity<JwtDTO> signIn(@Valid @RequestBody SignInDTO signInDTO) {
-        return ResponseEntity.ok(memberService.signIn(signInDTO));
+    public SingleResponse<JwtDTO> signIn(@Valid @RequestBody SignInDTO signInDTO) {
+        JwtDTO jwtDTO = memberService.signIn(signInDTO);
+        return responseService.getSingleResponse(jwtDTO);
     }
 
     /**
      * 토큰 재발행
      */
     @PostMapping("/reissue")
-    public ResponseEntity<JwtDTO> reissue(@RequestBody TokenDTO tokenDTO) {
-        return ResponseEntity.ok(memberService.reissue(tokenDTO));
+    public SingleResponse<JwtDTO> reissue(@RequestBody TokenDTO tokenDTO) {
+        JwtDTO jwtDTO = memberService.reissue(tokenDTO);
+        return responseService.getSingleResponse(jwtDTO);
     }
 
     /**
